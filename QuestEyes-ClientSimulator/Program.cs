bfeecf545694734dde1c05144552511bffd6689f
robotprobot <swheeler2013@outlook.com>
2022-05-02 22:36:08 +0100

    Improvements to connection system

--- QuestEyes-ClientSimulator/Program.cs
+++ QuestEyes-ClientSimulator/Program.cs
@@ -4,38 +4,27 @@ using System.Net.Sockets;
 using System.Text;
 using Timer = System.Timers.Timer;
 
-bool connecting = false;
+bool connecting = true;
 bool connected = false;
+Timer udpTimer = new();
 
 //print info
-Console.Title = "QuestEyes Client Simulator";
-Console.WriteLine("QuestEyes Client Simulator");
+Console.Title = "QuestEyes Device Simulator";
+Console.WriteLine("QuestEyes Device Simulator by Steven Wheeler");
 Console.WriteLine("This program simulates a QuestEyes device for development purposes.");
+
+//prep the UDP client and communication socket
+UdpClient discoveryUDP = new();
+TcpListener communicationSocket = new(IPAddress.Parse("127.0.0.1"), 7580);
 
 //generate a device identifier prefaced with SIM
 int _min = 0001;
 int _max = 9999;
-Random _rdm = new Random();
+Random _rdm = new();
 int identifier = _rdm.Next(_min, _max);
-string simDeviceName = "QuestEyes-SIM" + identifier.ToString();
+string simDeviceName = "SIM" + identifier.ToString();
 Console.WriteLine("\nSimulating as: " + simDeviceName);
-Console.Title = "QuestEyes Client Simulator - " + simDeviceName;
-
-//Get local IP
-string localIP = string.Empty;
-string strHostName = Dns.GetHostName();
-
-IPHostEntry ipEntry = Dns.GetHostEntry(strHostName);
-IPAddress[] addr = ipEntry.AddressList;
-for (int i = 0; i < addr.Length; i++)
-{
-    if (!addr[i].ToString().Contains(":"))
-    {
-         localIP = addr[i].ToString();
-         Console.WriteLine("Local IP: " + localIP + "\n");
-    }
-}
-Console.Title = "QuestEyes Client Simulator - " + simDeviceName + " - " + localIP;
+Console.Title = "QuestEyes Device Simulator - " + simDeviceName;
 
 //identify the images to be looped over the feed
 string exePath = Assembly.GetExecutingAssembly().Location;
@@ -53,36 +42,53 @@ if (detectedAmount == 0)
 {
     Console.WriteLine("No .JPG images found in Images folder. Please note that subfolders are ignored. Exiting...");
     Environment.Exit(0);
-} else
+}
+else
 {
     Console.WriteLine("Found " + detectedAmount + " image(s) to loop over connection.");
-}
-
-//Begin the connection to the server, like a actual QuestEyes device would
-try
-{
-    Console.WriteLine("Broadcasting towards listening server applications...");
-    Socket udp = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
-    IPAddress serverAddr = IPAddress.Parse("255.255.255.255");
-    IPEndPoint endPoint = new IPEndPoint(serverAddr, 7579);
-    Byte[] broadcastString = Encoding.ASCII.GetBytes("QUESTEYE_REQ_CONN:" + simDeviceName + ":" + localIP);
-    udp.EnableBroadcast = true;
-
-    Timer udpTimer = new();
-    udpTimer.Elapsed += (sender, e) => udpSend(sender, e, connecting, connected, udp, broadcastString, endPoint);
+    //Begin broadcasting presence for the server, like a actual QuestEyes device would
+    udpTimer.Elapsed += (sender, e) => udpSend(discoveryUDP, connected, simDeviceName);
     udpTimer.Interval = 1000;
+    communicationSocket.Start();
     udpTimer.Start();
-
-} catch (Exception e) {
-    Console.WriteLine(e);
 }
 
-static void udpSend(object sender, EventArgs e, bool connecting, bool connected, Socket udp, Byte[] broadcastString, IPEndPoint endPoint)
+Console.WriteLine("Broadcasting towards listening server applications on local device...");
+Console.WriteLine("Communication socket is open and waiting for connection.");
+
+while (connecting) {
+    Console.WriteLine("Waiting for connection...");
+    TcpClient client = communicationSocket.AcceptTcpClient();
+    Console.WriteLine("Connection received.");
+    connecting = true;
+    NetworkStream stream = client.GetStream();
+    string sendName = "NAME QuestEyes-" + simDeviceName;
+    string sendFirmware = "FIRMWARE_VER SIMULATOR";
+    byte[] msg = System.Text.Encoding.ASCII.GetBytes(sendName);
+    stream.Write(msg, 0, msg.Length);
+    Console.WriteLine(String.Format("Sent: {0}", System.Text.Encoding.UTF8.GetString(msg)));
+    msg = System.Text.Encoding.ASCII.GetBytes(sendFirmware);
+    stream.Write(msg, 0, msg.Length);
+    Console.WriteLine(String.Format("Sent: {0}", System.Text.Encoding.UTF8.GetString(msg)));
+    connected = true;
+    connecting = false;
+    while (connected) { };
+}
+
+//timer for sending presence packets
+static void udpSend(UdpClient udpClient, bool connected, string simDeviceName)
 {
-    if (connecting == false && connected == false)
+    string broadcastString = "QUESTEYE_REQ_CONN:" + ("QuestEyes-" + simDeviceName) + ":127.0.0.1";
+    byte[] broadcastBytes = Encoding.ASCII.GetBytes(broadcastString);
+    try
     {
-        udp.SendTo(broadcastString, endPoint);
+        if (!connected)
+        {
+            udpClient.Send(broadcastBytes, broadcastBytes.Length, "127.0.0.1", 7579);
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.ToString());
     }
 }
-
-//setup websocket logic and handle connection
